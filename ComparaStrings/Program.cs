0e45925bfc9cd5d0ba7a347af9581c0a3b596997
GBFerro <giovani.ferro@usp.br>
2023-03-07 20:39:08 -0300

    Add project files.

---
+++ ComparaStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿string string1, string2;
+
+string LerString()
+{
+    Console.WriteLine("Digite um texto: ");
+    return Console.ReadLine();
+}
+
+void ComparaTextos(string root, string root2)
+{
+    bool result = root.Equals(root2);
+    string aux;
+    if (result)
+    {
+        aux = "iguais";
+    }
+    else 
+    {
+        aux = "diferentes";
+    }
+    Console.WriteLine($"Os textos <{root}> e <{root2}> são {aux}");
+}
+
+string1 = LerString();
+string2 = LerString();
+
+ComparaTextos(string1, string2);
